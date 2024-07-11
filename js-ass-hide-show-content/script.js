4f9bed38fb064181d6e91d153d22149c8fff6e35
Mahitab Zakaria Elbehery <56124273+mahitab77@users.noreply.github.com>
2024-07-11 14:24:49 +0300

    Add files via upload

---
+++ js-ass-hide-show-content/script.js
@@ -0,0 +1,7 @@
+document.getElementById('showBtn').addEventListener('click', function() {
+  document.getElementById('content').style.display = 'block';
+});
+
+document.getElementById('hideBtn').addEventListener('click', function() {
+  document.getElementById('content').style.display = 'none';
+});
